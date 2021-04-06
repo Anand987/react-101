877ed6952830a5016f5915e43fc312ea0a29c80c
Anand987 <anandsharma110397@gmail.com>
2021-04-06 11:51:45 +0530

    Backdrop Added successfully

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,3 +1,8 @@
-const Backdrop = (props) => {};
+import classes from "./Backdrop.module.css";
+
+const Backdrop = (props) =>
+  props.show ? (
+    <div className={classes.Backdrop} onClick={props.clicked}></div>
+  ) : null;
 
 export default Backdrop;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,15 +1,20 @@
 import classes from "./Modal.module.css";
+import Auxiliary from "../../../hoc/Auxiliary";
+import Bakdrop from "../Backdrop/Backdrop";
 
 const Modal = (props) => (
-  <div
-    className={classes.Modal}
-    style={{
-      transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-      opacity: props.show ? 1 : 0,
-    }}
-  >
-    {props.children}
-  </div>
+  <Auxiliary>
+    <Bakdrop show={props.show} clicked={props.modalClosed} />
+    <div
+      className={classes.Modal}
+      style={{
+        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+        opacity: props.show ? 1 : 0,
+      }}
+    >
+      {props.children}
+    </div>
+  </Auxiliary>
 );
 
 export default Modal;
