347c117abd51caab90bdf411d37aa8b6bb90259e
Anand987 <anandsharma110397@gmail.com>
2021-04-07 13:29:42 +0530

    SideDrawer Added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,30 @@
+import { Component } from "react";
 import Auxiliary from "../../hoc/Auxiliary";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const Layout = (props) => (
-  <Auxiliary>
-    <Toolbar />
-    <main className={classes.Content}>{props.children}</main>
-  </Auxiliary>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
+
+  sideDrawerCloseHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      <Auxiliary>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerCloseHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Auxiliary>
+    );
+  }
+}
 
 export default Layout;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,7 +2,7 @@ import BurgerLogo from "../../assets/images/burger-logo.png";
 import classes from "./Logo.module.css";
 
 const Logo = (props) => (
-  <div className={classes.Logo}>
+  <div className={classes.Logo} style={{ height: props.height }}>
     <img src={BurgerLogo} alt="Burger Logo" />
   </div>
 );
