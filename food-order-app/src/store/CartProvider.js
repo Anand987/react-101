3e347198373bc10caf4053833fa1da8c5d735db2
Anand987 <anandsharma110397@gmail.com>
2021-07-30 14:49:13 +0530

    created cart context

---
+++ food-order-app/src/store/CartProvider.js
@@ -0,0 +1,23 @@
+// Goal of this component is to manage the cartContext data and provide that context to all components that wants access to it.
+
+import CartContext from "./cart-context";
+
+const CartProvider = (props) => {
+  const addItemToCartHandler = (item) => {};
+
+  const removeItemFromCartHandler = (id) => {};
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
