6acb4418dfd9d7420fdea8377300c1b9f2232107
Anand987 <anandsharma110397@gmail.com>
2022-01-08 17:46:11 +0530

    added new project

--- react_htttp_101/src/App.js
+++ react_htttp_101/src/App.js
@@ -1,32 +1,97 @@
-import React from 'react';
+import React, { useEffect, useCallback, useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  // Default GET request will get send.
+  // function fetchMoviesHandler() {
+  //   fetch("https://swapi.dev/api/films")
+  //     .then((response) => {
+  //       console.log(response);
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       const transformedData = data.results.map((movieData) => {
+  //         return {
+  //           id: movieData.episode_id,
+  //           title: movieData.title,
+  //           openingText: movieData.opening_crawl,
+  //           releaseDate: movieData.release_date,
+  //         };
+  //       });
+  //       setMovies(transformedData);
+  //     });
+
+  // useEffect(() => {
+  //   fetchMoviesHandler();
+  // },[fetchMoviesHandler]) // -> By doing this we are creating an infinite loop.
+
+  const fetchMoviesHandler = useCallback(async () => {
+    // Hitting URL getting response
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        "https://react-http-d20e2-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json"
+      );
+      if (!response.ok) {
+        throw Error("Something went wrong!");
+      }
+
+      // converting reponse promise to json
+      const data = await response.json();
+
+      const transformedData = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedData);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  const addMovieHandler = (movies) => {
+    console.log(movies);
+  };
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  let content = "Found no movies";
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = error;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
