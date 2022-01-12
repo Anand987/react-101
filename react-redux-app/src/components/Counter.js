c7b6a604fad552ab0b89b75df4c46d1bd1d9ebad
Anand987 <anandsharma110397@gmail.com>
2022-01-12 22:48:07 +0530

    update: created counter project
    to learn how to use react-redux and redux in react project

---
+++ react-redux-app/src/components/Counter.js
@@ -0,0 +1,35 @@
+import { useSelector, useDispatch } from "react-redux";
+
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  // useSelectore gives the state value from store
+  const counter = useSelector((state) => state.counter);
+
+  // calls a dispatch funciton from react-redux to use dipatch funciton in store.
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
