50fe6158b786c3c8fabd35b02dd37f9819663f48
Anand987 <anandsharma110397@gmail.com>
2022-01-12 23:14:30 +0530

    update: handle multiple state in redux

--- react-redux-app/src/components/Counter.js
+++ react-redux-app/src/components/Counter.js
@@ -5,6 +5,7 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   // useSelectore gives the state value from store
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   // calls a dispatch funciton from react-redux to use dipatch funciton in store.
   const dispatch = useDispatch();
@@ -17,15 +18,27 @@ const Counter = () => {
     dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
+
+  const decreasehandler = () => {
+    dispatch({ type: "decrease", amount: 5 });
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={decreasehandler}>Decrease by 5</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
