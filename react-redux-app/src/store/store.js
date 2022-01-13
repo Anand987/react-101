18a8d82edb3f24f6d827d115058468819138977d
Anand987 <anandsharma110397@gmail.com>
2022-01-13 23:24:25 +0530

    update: added redux/toolkit
    using new way to use store by using toolkit of redux

--- react-redux-app/src/store/store.js
+++ react-redux-app/src/store/store.js
@@ -1,6 +1,29 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    decrease(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
